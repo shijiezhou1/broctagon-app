1a65494dadef686633f0c6553377e7f3a2d300c3
Shijie Zhou <cs5129606@gmail.com>
2019-12-08 09:59:59 -0700

    login page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="loginItem">
+    <h1>Login</h1>
+    <md-field class="sd">
+      <label>Username</label>
+      <md-input class="inputItem" v-model="username"></md-input>
+    </md-field>
+
+    <md-field>
+      <label>Password</label>
+      <md-input class="inputItem" v-model="password"></md-input>
+    </md-field>
+
+    <md-button class="md-raised md-primary" v-on:click="login">Login</md-button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      if (this.username != "" && this.password != "") {
+        this.$router.push("market-place");
+      } else {
+        alert("Incorrect username and password!");
+      }
+    }
   }
 };
 </script>
+
+<style lang="css" scoped>
+.loginItem {
+  margin: auto;
+  width: 233px;
+  height: 74px;
+  vertical-align: middle;
+}
+.inputItem {
+  border-bottom: 1px solid lightgrey !important;
+}
+</style>
